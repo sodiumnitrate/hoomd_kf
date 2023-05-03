3b110cc7f20208c46a2567378a5cead66e597556
sodiumnitrate <irem.altan@gmail.com>
2023-05-03 12:06:18 -0400

    generating patch geometries

--- hoomd_kf/utils.py
+++ hoomd_kf/utils.py
@@ -61,3 +61,49 @@ def slice_to_indices(key, n_el):
         stop = key.stop
 
     return list(range(start, stop, step))
+
+def generate_patch_geometry(n_patches, theta=None):
+    """
+    Function to generate some basic patch geometries.
+    Works until N=4:
+    - N=1, single patch
+    - N=2, two patches, can be separated by an angle theta, if provided.
+    - N=3, 3 patches forming a triangle at the equator
+    - N=4, tetrahedral arrangement
+    """
+    # TODO: implement N=5 and N=6 as well
+    if n_patches > 4:
+        print("ERROR: can't generate patch geometry beyond n_patch=6.")
+        return None
+    if n_patches > 2 and theta is not None:
+        print("WARNING: I can only generate regular shapes beyond n_patch=2, ignoring theta.")
+    if n_patches == 1:
+        return [[1,0,0]]
+    if n_patches == 2:
+        if theta is None:
+            vec1 = [1,0,0]
+            vec2 = [-1,0,0]
+            vectors = [vec1, vec2]
+            return vectors
+        else:
+            vec1 = [1,0,0]
+            vec2 = [np.cos(theta), np.sin(theta), 0]
+            vectors = [vec1, vec2]
+            return vectors
+    elif n_patches == 3:
+        theta = 2*np.pi/3
+        vec1 = [1,0,0]
+        vec2 = [np.cos(theta),0,np.sin(theta)]
+        vec3 = [np.cos(theta),0,-np.sin(theta)]
+        return [vec1, vec2, vec3]
+    elif n_patches == 4:
+        vec1 = [1,0,0]
+        cos_theta = -1/3
+        theta = np.arccos(cos_theta)
+        vec2 = [np.cos(theta), 0, np.sin(theta)]
+        v3z = (np.cos(theta)-np.cos(theta)**2)/np.sin(theta)
+        v3y = np.sqrt(1-np.cos(theta)**2-v3z**2)
+        vec3 = [np.cos(theta), v3y, v3z]
+        vec4 = [np.cos(theta), -v3y, v3z]
+        vectors = [vec1, vec2, vec3, vec4]
+        return vectors
--- unit_tests/test_utils.py
+++ unit_tests/test_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from hoomd_kf.utils import check_adjacency
+from hoomd_kf.utils import generate_patch_geometry
 
 class TestUtils:
     def test_check_adjacency(self):
@@ -14,3 +15,57 @@ class TestUtils:
 
         adjacency = [[1,0],[0,1]]
         assert check_adjacency(adjacency)
+
+    def test_generate_patch_geometry_1(self):
+        vecs = generate_patch_geometry(1)
+        assert len(vecs) == 1
+        assert vecs[0][0] == 1
+        assert vecs[0][1] == 0
+        assert vecs[0][2] == 0
+
+    def test_generate_patch_geometry_2(self):
+        vecs = generate_patch_geometry(2)
+        assert len(vecs) == 2
+        v1 = np.array(vecs[0])
+        v2 = np.array(vecs[1])
+        assert v1.dot(v2) == -1
+        assert v1.dot(v1) == 1
+        assert v2.dot(v2) == 1
+
+    def test_generate_patch_geometry_3(self):
+        theta = 2.099
+        vecs = generate_patch_geometry(2,theta=theta)
+        assert len(vecs) == 2
+        v1 = np.array(vecs[0])
+        v2 = np.array(vecs[1])
+        assert v1.dot(v2) == np.cos(theta)
+        assert v1.dot(v1) == 1
+        assert v2.dot(v2) == 1
+
+    def test_generate_patch_geometry_4(self):
+        vecs = generate_patch_geometry(3)
+        assert len(vecs) == 3
+        v1 = np.array(vecs[0])
+        v2 = np.array(vecs[1])
+        v3 = np.array(vecs[2])
+        assert v1.dot(v2) == np.cos(2*np.pi/3)
+        assert v1.dot(v3) == np.cos(2*np.pi/3)
+        assert v1.dot(v1) == 1
+        assert v2.dot(v2) == 1
+        assert v3.dot(v3) == 1
+
+    def test_generate_patch_geometry_5(self):
+        vecs = generate_patch_geometry(4)
+        assert len(vecs) == 4
+        v1 = np.array(vecs[0])
+        v2 = np.array(vecs[1])
+        v3 = np.array(vecs[2])
+        v4 = np.array(vecs[3])
+
+        assert np.isclose(v1.dot(v2),-1./3)
+        assert np.isclose(v1.dot(v3),-1./3)
+        assert np.isclose(v1.dot(v4),-1./3)
+        assert v1.dot(v1) == 1
+        assert v2.dot(v2) == 1
+        assert v3.dot(v3) == 1
+        assert v4.dot(v4) == 1
